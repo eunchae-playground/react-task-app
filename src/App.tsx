884d9a3e60bad3fcaea3039cff4f7952bdc18af8
Eunchae Kim <kec9706@gmail.com>
2024-05-17 23:17:11 +0900

    5.17 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,87 @@
 import { useState } from "react";
+import { v4 } from "uuid";
 import "./App.css.ts";
-import { appContainer, board, buttons } from "./App.css.ts";
+import {
+  appContainer,
+  board,
+  buttons,
+  deleteBoardButton,
+  loggerButton,
+} from "./App.css.ts";
 import BoardList from "./components/BoardList/BoardList.tsx";
+import EditModal from "./components/EditModal/EditModal.tsx";
 import ListsContainer from "./components/ListsContainer/ListsContainer.tsx";
-import { useTypedSelector } from "./hooks/redux.ts";
+import LoggerModal from "./components/LoggerModal/LoggerModal.tsx";
+import { useTypedDispatch, useTypedSelector } from "./hooks/redux.ts";
+import { deleteBoard } from "./store/slices/boardsSlice.ts";
+import { addLog } from "./store/slices/loggerSlice.ts";
 
 function App() {
-  const [activeBoardId, setActiveBoardId] = useState("board-0");
+  const dispatch = useTypedDispatch();
 
-  const boards = useTypedSelector(state => state.boards.boardArray)
-  const activeBoard = boards.filter(board => board.boardId === activeBoardId)[0]
+  const [isLoggerOpen, setIsLoggerOpen] = useState(false);
+  const [activeBoardId, setActiveBoardId] = useState("board-0");
+  const modalActive = useTypedSelector((state) => state.boards.modalActive);
+  const boards = useTypedSelector((state) => state.boards.boardArray);
+  const activeBoard = boards.filter(
+    (board) => board.boardId === activeBoardId
+  )[0];
+
+  const handleDeleteBoard = () => {
+    if (boards.length > 1) {
+      dispatch(deleteBoard({ boardId: activeBoard.boardId }));
+
+      dispatch(
+        addLog({
+          logId: v4(),
+          logMessage: `게시판 지우기 ${activeBoard.boardName}`,
+          logAuthor: "User",
+          logTimestamp: String(Date.now()),
+        })
+      );
+
+      const newIndexToSet = () => {
+        const indexToBeDeleted = boards.findIndex(
+          (board) => board.boardId === activeBoardId
+        );
+        return indexToBeDeleted === 0
+          ? indexToBeDeleted + 1
+          : indexToBeDeleted - 1;
+      };
+
+      setActiveBoardId(boards[newIndexToSet()].boardId);
+    } else {
+      alert("최소 게시판 개수는 한 개 입니다.");
+    }
+  };
+
   return (
     <div className={appContainer}>
+      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
+      {modalActive ? <EditModal /> : null}
+
       <BoardList
         activeBoardId={activeBoardId}
         setActiveBoardId={setActiveBoardId}
       />
 
       <div className={board}>
-        <ListsContainer lists={activeBoard.lists} boardId={activeBoard.boardId}/>
+        <ListsContainer
+          lists={activeBoard.lists}
+          boardId={activeBoard.boardId}
+        />
       </div>
 
       <div className={buttons}>
-        <button>이 게시판 삭제하기</button>
-        <button>활동 목록 보이기</button>
+        <button className={deleteBoardButton} onClick={handleDeleteBoard}>
+          이 게시판 삭제하기
+        </button>
+        <button
+          className={loggerButton}
+          onClick={() => setIsLoggerOpen(!isLoggerOpen)}
+        >
+          {isLoggerOpen ? "활동 목록 숨기기" : "활동 목록 보이기"}
+        </button>
       </div>
     </div>
   );
--- src/components/LoggerModal/LogItem/LogItem.tsx
+++ src/components/LoggerModal/LogItem/LogItem.tsx
@@ -1,5 +1,29 @@
-const LogItem = () => {
-  return <div>LogItem</div>;
+import { BsFillPersonFill } from "react-icons/bs";
+import { ILogItem } from "../../../types";
+import { author, date, logItemWrap, message } from "./LogItem.css";
+
+type TLogItemProps = {
+  logItem: ILogItem;
+};
+
+const LogItem = ({ logItem }: TLogItemProps) => {
+  const timeOffset = new Date(Date.now() - Number(logItem.logTimestamp));
+  const showOffsetTime = `
+  ${timeOffset.getMinutes() > 0 ? `${timeOffset.getMinutes()}m` : ""}
+  ${timeOffset.getSeconds() > 0 ? `${timeOffset.getSeconds()}s ago` : ""}
+  ${timeOffset.getSeconds() === 0 ? `just now` : ""}
+  `;
+
+  return (
+    <div className={logItemWrap}>
+      <div className={author}>
+        <BsFillPersonFill />
+        {logItem.logAuthor}
+      </div>
+      <div className={message}>{logItem.logMessage}</div>
+      <div className={date}>{showOffsetTime}</div>
+    </div>
+  );
 };
 
 export default LogItem;
